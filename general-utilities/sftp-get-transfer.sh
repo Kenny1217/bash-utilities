99ab89cf0f66364adddfb30b637262c28c61f738
Kenny Morris <kmorris1217@gmail.com>
2024-11-19 16:45:27 -0500

    Create SFTP PUT and GET scripts

---
+++ general-utilities/sftp-get-transfer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $# -ne 5 ]]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+SFTP_USER="$1"
+SFTP_HOST="$2"
+REMOTE_DIRECTORY="$3"
+REMOTE_FILE="$4"
+LOCAL_DIRECTORY="$5"
+
+SFTP_CONNECTION="$SFTP_USER@$SFTP_HOST"
+
+sftp "$SFTP_CONNECTION" <<EOF
+    get "$REMOTE_DIRECTORY/$REMOTE_FILE $LOCAL_DIRECTORY"
+EOF
+
+SFTP_RETURN_CODE=$?
+
+if [ $SFTP_RETURN_CODE -eq 0 ]; then
+    echo "SFTP transfer complete"
+else
+    echo "SFTP transfer failed with exit code $SFTP_RETURN_CODE"
+fi
---
+++ general-utilities/sftp-put-transfer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [[ $# -ne 5 ]]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+SFTP_USER="$1"
+SFTP_HOST="$2"
+LOCAL_DIRECTORY="$3"
+LOCAL_FILE="$4"
+REMOTE_DIRECTORY="$5"
+
+SFTP_CONNECTION="$SFTP_USER@$SFTP_HOST"
+
+sftp "$SFTP_CONNECTION" <<EOF
+    put "$LOCAL_DIRECTORY/$LOCAL_FILE $REMOTE_DIRECTORY"
+EOF
+
+SFTP_RETURN_CODE=$?
+
+if [ $SFTP_RETURN_CODE -eq 0 ]; then
+    echo "SFTP transfer complete"
+else
+    echo "SFTP transfer failed with exit code $SFTP_RETURN_CODE"
+fi

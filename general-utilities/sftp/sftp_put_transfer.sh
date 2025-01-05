ca26c85470ca2da51f9f5c738bca31eac20e8dc9
Kenny Morris <kmorris1217@gmail.com>
2025-01-05 16:49:00 -0500

    create sftp put script

---
+++ general-utilities/sftp/sftp_put_transfer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SFTP_USER="$1"
+SFTP_HOST="$2"
+LOCAL_DIRECTORY="$3"
+LOCAL_DIRECTORY="$4"
+REMOTE_DIRECTORY="$5"
+REMOTE_FILE="$6"
+
+SFTP_CONNECTION="$SFTP_USER@$SFTP_HOST"
+
+sftp "$SFTP_CONNECTION" <<EOF
+    put "$LOCAL_DIRECTORY/$LOCAL_DIRECTORY $REMOTE_DIRECTORY/$REMOTE_FILE"
+EOF
+
+SFTP_RETURN_CODE=$?
+
+if [ $SFTP_RETURN_CODE -eq 0 ]; then
+    echo "SFTP transfer complete"
+else
+    echo "SFTP transfer failed with exit code $SFTP_RETURN_CODE"
+fi

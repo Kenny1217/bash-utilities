d2cf39704e03552c1f07fdfdd4ebff7dfd38f276
Kenny Morris <kmorris1217@gmail.com>
2025-01-05 18:45:28 -0500

    create some s3 scripts

---
+++ general-utilities/s3/s3_delete_bucket.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if ! command -v s3cmd &> /dev/null; then
+    echo "Error: s3cmd is required but is not installed. Please install s3cmd to continue"
+    exit 1
+fi
+
+if [[ $# -ne 1 ]]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+S3_BUCKET="$1"
+
+S3_FULL_PATH="s3://$S3_BUCKET"
+
+if ! s3cmd rb "$S3_FULL_PATH"; then
+    echo "Failed to delete s3 bucket $S3_BUCKET"
+    exit 1
+else
+    echo "Bucket $S3_BUCKET deleted"
+fi

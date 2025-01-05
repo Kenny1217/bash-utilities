5654f81c5be84808359f428af2eb3cfeaa0664e3
Kenny Morris <kmorris1217@gmail.com>
2025-01-05 18:32:45 -0500

    create script that runs shell script tests

---
+++ general-utilities/run_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+CURRENT_DIRECTORY="( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TEST_DIRECTORY="$CURRENT_DIRECTORY/tests"
+TEST_FILES="$TEST_DIRECTORY/*.sh"
+
+echo "Tests running"
+
+for TEST in $TEST_FILES; do 
+    . $TEST
+done
+
+echo "All tests completed"

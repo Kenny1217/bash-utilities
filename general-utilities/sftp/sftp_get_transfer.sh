6dd1666775a5e23c6c81755bb4142143bb0fb61d
Kenny Morris <kmorris1217@gmail.com>
2025-01-05 16:56:13 -0500

    create sftp get script

---
+++ general-utilities/sftp/sftp_get_transfer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SFTP_USER="$1"
+SFTP_HOST="$2"
+REMOTE_DIRECTORY="$3"
+REMOTE_FILE="$4"
+LOCAL_DIRECTORY="$5"
+LOCAL_FILE="$6"
+
+SFTP_CONNECTION="$SFTP_USER@$SFTP_HOST"
+
+sftp "$SFTP_CONNECTION" <<EOF
+    get "$REMOTE_DIRECTORY/$REMOTE_FILE $LOCAL_DIRECTORY/$LOCAL_FILE"
+EOF
+
+SFTP_RETURN_CODE=$?
+
+if [ $SFTP_RETURN_CODE -eq 0 ]; then
+    echo "SFTP transfer complete"
+else
+    echo "SFTP transfer failed with exit code $SFTP_RETURN_CODE"
+fi

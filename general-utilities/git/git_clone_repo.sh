8f112acd63a079f2c1bbdfd322585e827ac05aaa
Kenny Morris <kmorris1217@gmail.com>
2025-01-09 22:32:06 -0500

    add git clone script

---
+++ general-utilities/git/git_clone_repo.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if ! command -v git &> /dev/null; then
+    echo "Error: git is required but is not installed. Please install git to continue"
+    exit 1
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+GIT_REPO="$1"
+GIT_BRANCH="$2"
+GIT_CLONE_TRIES=0
+GIT_CLONE_MAX_TRIES=3
+
+echo "Attempting to clone branch $GIT_BRANCH from $GIT_REPO"
+
+while [ $GIT_CLONE_TRIES -lt $GIT_CLONE_MAX_TRIES ]; d0
+    GIT_CLONE_TRIES=$((GIT_CLONE_TRIES + 1))
+    echo "Attempt $GIT_CLONE_TRIES of $GIT_CLONE_MAX_TRIES"
+    if git clone --recurse-submodules -b "$GIT_BRANCH" "$GIT_REPO"; then
+        echo "Git clone successful"
+        break
+    else
+        echo "Git clone failed"
+    fi
+done
+
+if [ $GIT_CLONE_TRIES -eq $GIT_CLONE_MAX_TRIES ]; then
+    echo "Max attempts reached. Exiting..."
+fi

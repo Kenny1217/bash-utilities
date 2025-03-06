9b9f3d639452c3b3b24b06b134e2208006fc329d
Kenny Morris <kmorris1217@gmail.com>
2025-03-06 18:36:48 -0500

    single file get transfer

---
+++ general-utilities/s3/s3_get_file_transfer.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if ! command -v s3cmd &> /dev/null; then
+    echo "Error: s3cmd is required but it is not installed. Please install s3cmd to continue"
+    exit 1
+fi
+
+S3_BUCKET="$1"
+S3_PREFIX="$2"
+S3_OBJECT="$3"
+LOCAL_DIRECTORY="$4"
+LOCAL_FILENAME="$5"
+
+S3_FULL_PATH="s3://$S3_BUCKET/$S3_PREFIX/$S3_OBJECT"
+
+if ! s3cmd get "$S3_FULL_PATH" "$LOCAL_DIRECTORY/$LOCAL_FILENAME"; then
+    echo "Failed to transfer $S3_FULL_PATH"
+    exit 1
+else
+    echo "Transfer $S3_FULL_PATH completed"
+fi

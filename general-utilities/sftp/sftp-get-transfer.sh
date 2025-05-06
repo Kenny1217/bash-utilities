a0e549eebd944d69f5ecfb07a949654d45a0d6f5
Kenny Morris <kmorris1217@gmail.com>
2025-05-05 21:29:14 -0400

    add sftp scripts

---
+++ general-utilities/sftp/sftp-get-transfer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $# -ne 6 ]]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+SFTP_USER="$1"
+SFTP_HOST="$2"
+REMOTE_DIRECTORY="$3"
+REMOTE_FILENAME="$4"
+LOCAL_DIRECTORY="$5"
+LOCAL_FILENAME="$6"
+
+SFTP_CONNECTION="$SFTP_USER@$SFTP_HOST"
+REMOTE_FULL_PATH="$REMOTE_DIRECTORY/$REMOTE_FILENAME"
+LOCAL_FULL_PATH="$LOCAL_DIRECTORY/$LOCAL_FILENAME"
+
+sftp "$SFTP_CONNECTION" <<EOF
+    get "$REMOTE_FULL_PATH" "$LOCAL_FULL_PATH"
+EOF
+
+SFTP_RETURN_CODE=$?
+
+if [ $SFTP_RETURN_CODE -eq 0 ]; then
+    echo "SFTP transfer complete"
+else 
+    echo "SFTP transfer failed with exit code $SFTP_RETURN_CODE"
+fi
---
+++ general-utilities/sftp/sftp-put-transfer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ $# -ne 6 ]]; then
+    echo "Wrong number of arguments"
+    exit 1
+fi
+
+SFTP_USER="$1"
+SFTP_HOST="$2"
+LOCAL_DIRECTORY="$3"
+LOCAL_FILENAME="$4"
+REMOTE_DIRECTORY="$5"
+REMOTE_FILENAME="$6"
+
+SFTP_CONNECTION="$SFTP_USER@$SFTP_HOST"
+LOCAL_FULL_PATH="$LOCAL_DIRECTORY/$LOCAL_FILENAME"
+REMOTE_FULL_PATH="$REMOTE_DIRECTORY/$REMOTE_FILENAME"
+
+sftp "$SFTP_CONNECTION" <<EOF
+    put "$LOCAL_FULL_PATH" "$REMOTE_FULL_PATH"
+EOF
+
+SFTP_RETURN_CODE=$?
+
+if [ $SFTP_RETURN_CODE -eq 0 ]; then
+    echo "SFTP transfer complete"
+else 
+    echo "SFTP transfer failed with exit code $SFTP_RETURN_CODE"
+fi
